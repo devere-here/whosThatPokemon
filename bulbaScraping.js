d7b2726551f849ef7adeb592cf5d26c6e112ae8d
Steven DeVere <sdevere@xogrp.com>
2018-10-04 00:00:05 -0400

    feat: scraped off all sprites for single pokemon type

--- bulbaScraping.js
+++ bulbaScraping.js
@@ -2,29 +2,33 @@ const rp = require('request-promise');
 const baseUrl = 'https://pokemondb.net'
 const typeExtension = '/type/grass';
 const $ = require('cheerio');
+const sharp = require('sharp');
 
 
 rp(baseUrl + typeExtension)
 .then(function(html){
   //success!
-  const pokemonExtension = $('.infocard-md-data > a', html)[0].attribs.href;
-  console.log('bulbasaur', $('.infocard-md-data > a', html)[0].attribs.href)
+  const pokemonNames = $('a[class="ent-name"]', html);
+  const length = Object.keys(pokemonNames).length
 
-  rp(baseUrl + pokemonExtension)
-  .then(function(html){
-    const images = $('a[class="sprite-share-link "]', html);
-    const spriteUrls = [];
+  for (let i = 0; i < length; i++){
+    const pokemonName = $('a[class="ent-name"]', html)[i].children[0].data;
 
-    for (let i = 0; i < images.length; i++){
-      const url = images[i].attribs.href;
-      if (!url.includes('red-blue') && !url.includes('x-y')){
-        spriteUrls.push(url)
+    rp(baseUrl + '/sprites/' + pokemonName)
+    .then(function(html){
+      const images = $('a[class="sprite-share-link "]', html);
+      const spriteUrls = [];
+
+      for (let i = 0; i < images.length; i++){
+        const url = images[i].attribs.href;
+        if (!url.includes('shiny') && !url.includes('red-blue') && !url.includes('yellow')){
+          spriteUrls.push(url)
+        }
       }
-    }
+      console.log('saur!!!', spriteUrls);
+    })
 
-    console.log('saur!!!', spriteUrls);
-
-  })
+  }
 
 })
 .catch(function(err){
