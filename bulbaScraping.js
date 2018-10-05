81e39440f4884b07a3a98cce41c2ec5f84c218b4
Steven DeVere <sdevere@xogrp.com>
2018-10-05 16:00:59 -0400

    feat: can download an organize pokemon in multiple folders

--- bulbaScraping.js
+++ bulbaScraping.js
@@ -8,53 +8,41 @@ const { JSDOM } = jsdom;
 var PImage = require('pureimage');
 const fs = require('fs');
 const download = require('image-downloader');
+const shell = require('shelljs');
+let images = 0;
+let receivedImages = 0;
 
 
+const getPokemonImages = (pokemonName, type) => {
+  rp(baseUrl + '/sprites/' + pokemonName)
+  .then(function(html){
+    console.log('in the then in getPokemonImages')
+    const images = $('a[class="sprite-share-link "]', html);
+    const spriteUrls = [];
 
-rp(baseUrl + typeExtension)
-.then(function(html){
-  //success!
-  const pokemonNames = $('a[class="ent-name"]', html);
-  const length = Object.keys(pokemonNames).length
+    images += images.length;
 
-  let count = 0;
-
-  for (let i = 0; i < length; i++){
-    const pokemonName = $('a[class="ent-name"]', html)[i].children[0].data;
-
-    rp(baseUrl + '/sprites/' + pokemonName)
-    .then(function(html){
-      const images = $('a[class="sprite-share-link "]', html);
-      const spriteUrls = [];
-
-      for (let i = 0; i < images.length; i++){
-        const url = images[i].attribs.href;
-        if (!url.includes('shiny') && !url.includes('red-blue') && !url.includes('yellow')){
-          spriteUrls.push(url)
-          count++
-        }
+    for (let i = 0; i < images.length; i++){
+      const url = images[i].attribs.href;
+      if (!url.includes('shiny') && !url.includes('red-blue') && !url.includes('yellow')){
+        spriteUrls.push(url)
       }
-      console.log('saur!!!', spriteUrls);
+    }
+    console.log('saur!!!', spriteUrls);
 
-      return {
-        spriteUrls,
-        pokemonName
-      }
-    })
-    .then(({ spriteUrls, pokemonName }) => {
-      load(spriteUrls, pokemonName)
-    })
+    return {
+      spriteUrls,
+      pokemonName
+    }
+  })
+  .then(({ spriteUrls, pokemonName }) => {
+    load(spriteUrls, pokemonName, type)
+  })
 
-  }
-
-})
-.catch(function(err){
-  //handle error
-});
+}
 
 
-const load = (imageArray, name) => {
-  console.log('in load');
+const load = (imageArray, name, type) => {
   // const dom = new JSDOM("<div><img src='https://img.pokemondb.net/sprites/omega-ruby-alpha-sapphire/dex/normal/charizard-mega-y.png' /><canvas /></div>");
   // const ctx = dom.window.document.querySelector("canvas").getContext('2d');
   // const imgRequest = new Promise((resolve, reject) => {
@@ -62,25 +50,32 @@ const load = (imageArray, name) => {
   //   console.log('in the imgRequest');
   // })
 
-  fs.mkdirSync(`.${typeExtension}/${name}`)
+  (function(imageArray, name, type){
+    console.log(`./type/${type}/${name}`)
+    fs.mkdir(`./type/${type}/${name}`, (err) => {
+      if (err) {
+        console.log('error while making directory')
+      } else {
+        imageArray.forEach((url, idx) => {
+          const options = {
+            url: url,
+            dest: `./type/${type}/${name}/${idx}.png`
+          }
 
-  imageArray.forEach((url, idx) => {
-    const options = {
-      url: url,
-      dest: `./${name}/${idx}.png`
-    }
+          download.image(options)
+          .then(({ filename }) => {
+            console.log('File saved to', filename);
+            receivedImages++
+          })
+          .catch((err) => {
+            console.error(err)
+          })
 
-    download.image(options)
-    .then(({ filename }) => {
-      console.log('File saved to', filename)
+        })
+      }
     })
-    .catch((err) => {
-      console.error(err)
-    })
-
-  })
-
-
+  }
+  )(imageArray, name, type)
 
 
 
@@ -122,3 +117,32 @@ const load = (imageArray, name) => {
   // })
 
 }
+
+
+const getPokemonOfType = (type) => {
+  console.log('type', `${baseUrl}/type/${type}`)
+  rp(`${baseUrl}/type/${type}`)
+  .then(function(html){
+    //success!
+    console.log('in the then')
+
+    const pokemonNames = $('a[class="ent-name"]', html);
+    const length = Object.keys(pokemonNames).length
+
+    for (let i = 0; i < length; i++){
+      const pokemonName = $('a[class="ent-name"]', html)[i].children[0].data;
+      getPokemonImages(pokemonName, type);
+    }
+
+  })
+  .catch(function(err){
+    //handle error
+  });
+
+}
+
+const pokemonTypes = ['fire', 'water'];
+
+pokemonTypes.forEach(type => {
+  getPokemonOfType(type)
+})
